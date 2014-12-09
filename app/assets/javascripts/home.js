c5ffbea0ca553ba69db5d6607b8f3a3d459f1649
samfeldman <samantha.ariel.feldman@gmail.com>
2014-12-09 18:02:12 -0500

    Tweaked javascript, creacreated json output with ajax call

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -1,44 +1,82 @@
-$(document).ready (function () {
-	$( ".modal_box" ).on( "click", function() {
-    	$( "#back_block" ).hide();
-		});
-
+$(function() {
 	
+	$( ".modal_box" ).on( "click", function(){
+		$( "#back_block" ).hide();
+	});
 
+	var latitude = 0;
+	var longitude = 0;
+	var map;
+	var markers =[];
 
-
+	google.maps.event.addDomListener(window, 'load', initialize);
 
 	function initialize() {
-	    var mapDiv = document.getElementById('mapdiv');
-	    var mapOptions = {
-	      center: new google.maps.LatLng(40.720079, -73.949511),
-	      zoom: 16,
-	      mapTypeId: google.maps.MapTypeId.ROADMAP
-	    }
-	    var map = new google.maps.Map(mapDiv, mapOptions);
-	   	var image = new google.maps.MarkerImage('./images/drinkglass.png',
-			new google.maps.Size(129, 42),
-			new google.maps.Point(0,0),
-			new google.maps.Point(0,0)
-		);
-	   	var marker1 = new google.maps.Marker({
-			position: new google.maps.LatLng(40.720079, -73.949511),
-			map: map,
-			icon: image
-    	});
-		google.maps.event.addListener(marker1, 'click', function() {
-			infowindow1.open(map, marker1);
-		});
-		var infowindow1 = new google.maps.InfoWindow({
-			content:  createInfo('Whiskey Business ', 'McCarren Park Pool, 776 Lorimer St. Brooklyn, NY, 11222')
-		});	
-		function createInfo(title, content) {
-			return '<div class="infowindow"><strong>'+ title +'</strong>'+content+'</div>';
-		}
-	
+		var mapOptions = {
+			zoom: 9,
+			center: new google.maps.LatLng(latitude, longitude)
+		};
+
+		map = new google.maps.Map(document.getElementById('mapdiv'),
+	                          	  mapOptions);
 	};
-  	
-  	google.maps.event.addDomListener(window, 'load', initialize);
+
+	if ("geolocation" in navigator){
+		navigator.geolocation.getCurrentPosition(function(position){
+			latitude = position.coords.latitude;
+			longitude = position.coords.longitude;
+
+			var geocoder = new google.maps.Geocoder();
+			var address = "new york";
+			var lat1 = nil;
+			var lon1 = nil;
+
+			geocoder.geocode( { 'address': address}, function(results, status) {
+
+				if (status == google.maps.GeocoderStatus.OK) {
+				    lat1 = results[0].geometry.location.latitude;
+				    lon1 = results[0].geometry.location.longitude;
+				    alert(latitude);
+				} 
+			}); 
+		})
+	}
+
+	function dist_bet(lat1, lon1, lat2, lon2){
+
+		if (typeof(Number.prototype.toRad) === "undefined") {
+			Number.prototype.toRad = function() {
+		    return this * Math.PI / 180;
+			}
+		}
+
+		var R = 6371;
+		var φ1 = lat1.toRad();
+		var φ2 = lat2.toRad();
+		var Δφ = (lat2-lat1).toRad();
+		var Δλ = (lon2-lon1).toRad();
+
+		var a = Math.sin(Δφ/2) * Math.sin(Δφ/2) +
+		        Math.cos(φ1) * Math.cos(φ2) *
+		        Math.sin(Δλ/2) * Math.sin(Δλ/2);
+		var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1-a));
+
+		var d = R * c;
+
+		console.log(d);
+
+	};
+
+
+	$.ajax({
+		type: "GET",
+		url: "/show",
+	})
+	.done(function( object ) {
+		console.log(object)
+		alert( object.latitude );
+	});
+
 
 });
 
--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -2,18 +2,9 @@ class HomeController < ApplicationController
   def index
   end
 
-  def create
-	@user = User.new(user_params)
-	    if @user.save!
-	      @follow = Follower.new(follower_id: @user.id, leader_id: @user.id)
-	      @follow.save
-	      session[:user_id] = @user.id
-	      p "session saved"
-	      redirect_to "/home/#{@user.id}/dash"
-	    else 
-	      flash[:alert] = @user.errors.full_messages
-	      redirect_to :back
-	    end
+  def show
+  	@entrances = Entrance.all
+  	render :json => @entrances
   end
 
   private
